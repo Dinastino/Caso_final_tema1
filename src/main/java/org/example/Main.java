f0530a754d1dc98f82a481a46d27dce26aa6c1f4
Carlos <corammun@myuax.com>
2024-02-25 12:48:55 +0100

    8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        CronometroCosmico cronometroCosmico = new CronometroCosmico();
+        System.out.println(cronometroCosmico);
     }
 }
